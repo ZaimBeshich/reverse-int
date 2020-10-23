4f0a95e560147cdae22ac0f390c02981b72ee76e
Zaim <wtfgagarin@gmail.com>
2020-10-23 16:16:33 +0300

    feat: task solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (n > 0) {
+        return (String(n).split("").reverse().join("")) * 1;
+    } else {
+        let b = n * -1;
+        return String(b).split("").reverse().join("") * 1;
+    }
 }
